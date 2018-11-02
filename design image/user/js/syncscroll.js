f38ced20009dbec9d60288f085e30bc495b94de4
Shuuei Takahashi <takasyue1023sw@gmail.com>
2018-11-02 16:06:46 +0900

    save

--- design image/user/js/syncscroll.js
+++ design image/user/js/syncscroll.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
 
-    ('#time-area').scroll(function () {
+    //読込イベント処理を書く
 
-        $('#event-area').scrollTop($('#time-area').scrollTop());
+    var div1 = $('#time-area');
+    var div2 = $('#event-area');
+
+    div1.scroll(function () {
+
+        div2.scrollTop(div1.scrollTop());
 
     });
 
-    $('#event-area').scroll(function () {
+    div2.scroll(function () {
 
-        $('#time-area').scrollTop($('#event-area').scrollTop());
+        div1.scrollTop(div2.scrollTop());
     });
 
 });
