4e47fb084efbc111cf3a02beda76317674c2a64e
Shuuei Takahashi <takasyue1023sw@gmail.com>
2018-11-15 13:41:45 +0900

    for文での表示

--- application/src/servlet/User_Timetable.java
+++ application/src/servlet/User_Timetable.java
@@ -14,13 +14,13 @@ public class User_Timetable extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public User_Timetable() {
 		super();
-		}
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String view = "/WEB-INF/user/timetable.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
 		dispatcher.forward(request, response);
-		}
+	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 	}
