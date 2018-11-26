ab31130326cc99567a6b479845b7f11544867bc3
Shuuei Takahashi <takasyue1023sw@gmail.com>
2018-11-26 16:53:11 +0900

    ログインページのデータベース接続完了
    ログインの判定はbooleanを用いて、簡単に判定できるように作りました。
    DAOで取得したデータ数がなかった時、falseを返すようになっています。・

--- application/src/servlet/Login_Main.java
+++ application/src/servlet/Login_Main.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +10,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import culculator.Calc_con;
+import culculator.Calcurator;
+import dao.Login_DAO;
+import dao.User_SelectDAO;
+import dto.User_DTO;
 
 /**
  * Servlet implementation class Login_Main
@@ -16,18 +25,19 @@ import javax.servlet.http.HttpServletResponse;
 public class Login_Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login_Main() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login_Main() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("caution", "　");
 		String view = "/WEB-INF/login/login.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
 		dispatcher.forward(request, response);
@@ -37,9 +47,32 @@ public class Login_Main extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String view = "/WEB-INF/login/login.jsp";
+		request.setCharacterEncoding("UTF-8");
+		String view="";
+		if(Login_DAO.login(Integer.parseInt(request.getParameter("id")), request.getParameter("pass"))) {
+			HttpSession session = request.getSession();
+			session.setAttribute("userId", request.getParameter("id"));
+			//トップイベントリスト
+			ArrayList<User_DTO> hl = User_SelectDAO.top_event();
+			//ミドルイベントリスト
+			ArrayList<User_DTO> mel = User_SelectDAO.middle_event(1);
+			//ボトムイベントリスト
+			HashMap<Integer, ArrayList<User_DTO>> bel = User_SelectDAO.bottom_event();
+			//イベントや間隙の領域リスト
+			HashMap<Integer,ArrayList<Calc_con>> interval = Calcurator.time_interval(mel.size(), bel);
+			//セッション打ち上げ
+			session.setAttribute("hl", hl);
+			session.setAttribute("mel", mel);
+			session.setAttribute("bel", bel);
+			session.setAttribute("interval", interval);
+			view = "/WEB-INF/user/timetable.jsp";
+		}else {
+			request.setAttribute("caution", "学籍番号 または パスワードが間違っています。");
+			view = "/WEB-INF/login/login.jsp";
+		}
 		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
 		dispatcher.forward(request, response);
+
 	}
 
 }
