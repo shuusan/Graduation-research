503ee1765109a8513fe449276f2f8591ff0a635b
Shuuei Takahashi <takasyue1023sw@gmail.com>
2018-12-05 13:30:25 +0900

    デザインイメージでのテスト

--- design image/user/js/replace.js
+++ design image/user/js/replace.js
@@ -1,4 +1,16 @@
-$('.btn.btn-primary').on('click', function () {
-    var id = $(this).attr("id");
-    alert(id);
+$(document).ready(function () {
+    $('button').click(function () {
+        var val = $(this).attr('id');
+        var text = val.slice(val.length - 1);
+        var id1 = "#time" + text;
+        var id2 = "#title" + text;
+        var id3 = "#contents" + text;
+        var hoge1 = $(id1).text();
+        var hoge2 = $(id2).text();
+        var hoge3 = $(id3).text();
+        $('.bar-title').text(hoge2);
+        $('.bar-time').text(hoge1);
+        $('.bar-contents').text(hoge3);
+    })
+
 });
